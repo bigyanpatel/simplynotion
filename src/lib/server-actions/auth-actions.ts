e900a17cc6be01951cb5d6385c130e565a63eb36
Bigyan Darshan Patel <bigyanpatel07769@gmail.com>
2024-01-03 19:17:54 +0530

    Feat: add user signup server actions

--- src/lib/server-actions/auth-actions.ts
+++ src/lib/server-actions/auth-actions.ts
@@ -1,9 +1,9 @@
-'use server';
+"use server";
 
-import { z } from 'zod';
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
-import { FormSchema } from '../types';
-import { cookies } from 'next/headers';
+import { z } from "zod";
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { FormSchema } from "../types";
+import { cookies } from "next/headers";
 
 export async function actionLoginUser({
   email,
@@ -13,6 +13,27 @@ export async function actionLoginUser({
   const response = await supabase.auth.signInWithPassword({
     email,
     password,
+  });
+  return response;
+}
+
+export async function actionSignUpUser({
+  email,
+  password,
+}: z.infer<typeof FormSchema>) {
+  const supabase = createRouteHandlerClient({ cookies });
+  const { data } = await supabase
+    .from("profiles")
+    .select("*")
+    .eq("email", email);
+
+  if (data?.length) return { error: { message: "User already exists", data } };
+  const response = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      emailRedirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}api/auth/callback`,
+    },
   });
   return response;
 }
