db7bfd5c7d1f95957078b9172f6db4d3cb4b6c3e
Bigyan Darshan Patel <bigyanpatel07769@gmail.com>
2024-01-04 18:49:33 +0530

    Init: Base layout for the dashboard page

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from 'react';
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 
-const Page = () => {
-  return (
-    <div>Page</div>
-  )
-}
+import { cookies } from 'next/headers';
+import db from '@/lib/supabase/db';
+import { redirect } from 'next/navigation';
 
-export default Page
+const DashboardPage = async () => {
+  const supabase = createServerComponentClient({ cookies });
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) return;
+
+  const workspace = await db.query.workspaces.findFirst({
+    where: (workspace, { eq }) => eq(workspace.workspaceOwner, user.id),
+  });
+
+
+
+  if (!workspace)
+    return (
+      <div
+        className="bg-background
+        h-screen
+        w-screen
+        flex
+        justify-center
+        items-center
+  "
+      >
+        Basic Layout
+      </div>
+    );
+
+  redirect(`/dashboard/${workspace.id}`);
+};
+
+export default DashboardPage;
