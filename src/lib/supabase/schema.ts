bf0862f5be456bf23c4b4b114e2e772858604116
Bigyan Darshan Patel <bigyanpatel07769@gmail.com>
2024-01-07 01:59:37 +0530

    PATCH: schema pull for collaborators from supabase

--- src/lib/supabase/schema.ts
+++ src/lib/supabase/schema.ts
@@ -1,8 +1,16 @@
-import { boolean, integer, jsonb, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import {
+  boolean,
+  integer,
+  jsonb,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+} from "drizzle-orm/pg-core";
 import { prices, subscriptionStatus, users } from "../../../migrations/schema";
 import { and, eq, notExists, sql } from "drizzle-orm";
 import db from "./db";
-import { workspace } from "./supabase.types";
+import { User, workspace } from "./supabase.types";
 
 export const workspaces = pgTable("workspaces", {
   id: uuid("id").defaultRandom().primaryKey().notNull(),
@@ -21,83 +29,110 @@ export const workspaces = pgTable("workspaces", {
   bannerUrl: text("banner_url"),
 });
 
-export const folders = pgTable('folders', {
-    id: uuid('id').defaultRandom().primaryKey().notNull(),
-    createdAt: timestamp('created_at', {
-      withTimezone: true,
-      mode: 'string',
-    })
-      .defaultNow()
-      .notNull(),
-    title: text('title').notNull(),
-    iconId: text('icon_id').notNull(),
-    data: text('data'),
-    inTrash: text('in_trash'),
-    bannerUrl: text('banner_url'),
-    workspaceId: uuid('workspace_id')
-      .notNull()
-      .references(() => workspaces.id, {
-        onDelete: 'cascade',
-      }),
-  });
-  
-  export const files = pgTable('files', {
-    id: uuid('id').defaultRandom().primaryKey().notNull(),
-    createdAt: timestamp('created_at', {
-      withTimezone: true,
-      mode: 'string',
-    })
-      .defaultNow()
-      .notNull(),
-    title: text('title').notNull(),
-    iconId: text('icon_id').notNull(),
-    data: text('data'),
-    inTrash: text('in_trash'),
-    bannerUrl: text('banner_url'),
-    workspaceId: uuid('workspace_id')
-      .notNull()
-      .references(() => workspaces.id, {
-        onDelete: 'cascade',
-      }),
-    folderId: uuid('folder_id')
-      .notNull()
-      .references(() => folders.id, {
-        onDelete: 'cascade',
-      }),
-  });
-
-  export const subscriptions = pgTable("subscriptions", {
-	id: text("id").primaryKey().notNull(),
-	userId: uuid("user_id").notNull(),
-	status: subscriptionStatus("status"),
-	metadata: jsonb("metadata"),
-	priceId: text("price_id").references(() => prices.id),
-	quantity: integer("quantity"),
-	cancelAtPeriodEnd: boolean("cancel_at_period_end"),
-	created: timestamp("created", { withTimezone: true, mode: 'string' }).default(sql`now()`).notNull(),
-	currentPeriodStart: timestamp("current_period_start", { withTimezone: true, mode: 'string' }).default(sql`now()`).notNull(),
-	currentPeriodEnd: timestamp("current_period_end", { withTimezone: true, mode: 'string' }).default(sql`now()`).notNull(),
-	endedAt: timestamp("ended_at", { withTimezone: true, mode: 'string' }).default(sql`now()`),
-	cancelAt: timestamp("cancel_at", { withTimezone: true, mode: 'string' }).default(sql`now()`),
-	canceledAt: timestamp("canceled_at", { withTimezone: true, mode: 'string' }).default(sql`now()`),
-	trialStart: timestamp("trial_start", { withTimezone: true, mode: 'string' }).default(sql`now()`),
-	trialEnd: timestamp("trial_end", { withTimezone: true, mode: 'string' }).default(sql`now()`),
-});
-
-export const collaborators = pgTable('collaborators', {
-  id: uuid('id').defaultRandom().primaryKey().notNull(),
-  workspaceId: uuid('workspace_id')
-    .notNull()
-    .references(() => workspaces.id, { onDelete: 'cascade' }),
-  createdAt: timestamp('created_at', {
+export const folders = pgTable("folders", {
+  id: uuid("id").defaultRandom().primaryKey().notNull(),
+  createdAt: timestamp("created_at", {
     withTimezone: true,
-    mode: 'string',
+    mode: "string",
   })
     .defaultNow()
     .notNull(),
-  userId: uuid('user_id')
+  title: text("title").notNull(),
+  iconId: text("icon_id").notNull(),
+  data: text("data"),
+  inTrash: text("in_trash"),
+  bannerUrl: text("banner_url"),
+  workspaceId: uuid("workspace_id")
     .notNull()
-    .references(() => users.id, { onDelete: 'cascade' }),
+    .references(() => workspaces.id, {
+      onDelete: "cascade",
+    }),
+});
+
+export const files = pgTable("files", {
+  id: uuid("id").defaultRandom().primaryKey().notNull(),
+  createdAt: timestamp("created_at", {
+    withTimezone: true,
+    mode: "string",
+  })
+    .defaultNow()
+    .notNull(),
+  title: text("title").notNull(),
+  iconId: text("icon_id").notNull(),
+  data: text("data"),
+  inTrash: text("in_trash"),
+  bannerUrl: text("banner_url"),
+  workspaceId: uuid("workspace_id")
+    .notNull()
+    .references(() => workspaces.id, {
+      onDelete: "cascade",
+    }),
+  folderId: uuid("folder_id")
+    .notNull()
+    .references(() => folders.id, {
+      onDelete: "cascade",
+    }),
+});
+
+export const subscriptions = pgTable("subscriptions", {
+  id: text("id").primaryKey().notNull(),
+  userId: uuid("user_id").notNull(),
+  status: subscriptionStatus("status"),
+  metadata: jsonb("metadata"),
+  priceId: text("price_id").references(() => prices.id),
+  quantity: integer("quantity"),
+  cancelAtPeriodEnd: boolean("cancel_at_period_end"),
+  created: timestamp("created", { withTimezone: true, mode: "string" })
+    .default(sql`now()`)
+    .notNull(),
+  currentPeriodStart: timestamp("current_period_start", {
+    withTimezone: true,
+    mode: "string",
+  })
+    .default(sql`now()`)
+    .notNull(),
+  currentPeriodEnd: timestamp("current_period_end", {
+    withTimezone: true,
+    mode: "string",
+  })
+    .default(sql`now()`)
+    .notNull(),
+  endedAt: timestamp("ended_at", {
+    withTimezone: true,
+    mode: "string",
+  }).default(sql`now()`),
+  cancelAt: timestamp("cancel_at", {
+    withTimezone: true,
+    mode: "string",
+  }).default(sql`now()`),
+  canceledAt: timestamp("canceled_at", {
+    withTimezone: true,
+    mode: "string",
+  }).default(sql`now()`),
+  trialStart: timestamp("trial_start", {
+    withTimezone: true,
+    mode: "string",
+  }).default(sql`now()`),
+  trialEnd: timestamp("trial_end", {
+    withTimezone: true,
+    mode: "string",
+  }).default(sql`now()`),
+});
+
+export const collaborators = pgTable("collaborators", {
+  id: uuid("id").defaultRandom().primaryKey().notNull(),
+  workspaceId: uuid("workspace_id")
+    .notNull()
+    .references(() => workspaces.id, { onDelete: "cascade" }),
+  createdAt: timestamp("created_at", {
+    withTimezone: true,
+    mode: "string",
+  })
+    .defaultNow()
+    .notNull(),
+  userId: uuid("user_id")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
 });
 
 export const getPrivateWorkspaces = async (userId: string) => {
@@ -169,4 +204,15 @@ export const getSharedWorkspaces = async (userId: string) => {
     .innerJoin(collaborators, eq(workspaces.id, collaborators.workspaceId))
     .where(eq(workspaces.workspaceOwner, userId))) as workspace[];
   return sharedWorkspaces;
+};
+
+export const addCollaborators = async (users: User[], workspaceId: string) => {
+  const response = users.forEach(async (user: User) => {
+    const userExists = await db.query.collaborators.findFirst({
+      where: (u, { eq }) =>
+        and(eq(u.userId, user.id), eq(u.workspaceId, workspaceId)),
+    });
+    if (!userExists)
+      await db.insert(collaborators).values({ workspaceId, userId: user.id });
+  });
 };
