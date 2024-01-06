f40cc661620c1037104da1e6589326b4c1c9bd7f
Bigyan Darshan Patel <bigyanpatel07769@gmail.com>
2024-01-07 01:41:55 +0530

    Feat: init workspace creator with name input field

---
+++ src/components/global/workspace-creator.tsx
@@ -0,0 +1,57 @@
+'use client';
+import { useSupabaseUser } from '@/lib/providers/supabase-user-provider';
+import { User, workspace } from '@/lib/supabase/supabase.types';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
+import { Label } from '../ui/label';
+import { Input } from '../ui/input';
+import { useToast } from '../ui/use-toast';
+
+const WorkspaceCreator = () => {
+  const { user } = useSupabaseUser();
+  const { toast } = useToast();
+  const router = useRouter();
+  const [permissions, setPermissions] = useState('private');
+  const [title, setTitle] = useState('');
+  const [collaborators, setCollaborators] = useState<User[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const addCollaborator = (user: User) => {
+    setCollaborators([...collaborators, user]);
+  };
+
+  const removeCollaborator = (user: User) => {
+    setCollaborators(collaborators.filter((c) => c.id !== user.id));
+  };
+
+  return (
+    <div className="flex gap-4 flex-col">
+      <div>
+        <Label
+          htmlFor="name"
+          className="text-sm text-muted-foreground"
+        >
+          Name
+        </Label>
+        <div
+          className="flex 
+        justify-center 
+        items-center 
+        gap-2
+        "
+        >
+          <Input
+            name="name"
+            value={title}
+            placeholder="Workspace Name"
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WorkspaceCreator;
