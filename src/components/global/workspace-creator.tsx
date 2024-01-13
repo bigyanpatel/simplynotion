1686bcbbaa48a90b615fc88daecdb507ad76ebd6
Bigyan Darshan Patel <86798277+bigyanpatel@users.noreply.github.com>
2024-01-13 23:49:04 +0530

    Revert "Feat: private or shared workspace selector"
    This reverts commit 580494ff32994befc1126c8eddb10d29f1768776.

--- src/components/global/workspace-creator.tsx
+++ src/components/global/workspace-creator.tsx
@@ -1,27 +1,18 @@
-"use client";
-import { useSupabaseUser } from "@/lib/providers/supabase-user-provider";
-import { User, workspace } from "@/lib/supabase/supabase.types";
-import { useRouter } from "next/navigation";
-import React, { useState } from "react";
-import { Label } from "../ui/label";
-import { Input } from "../ui/input";
-import { useToast } from "../ui/use-toast";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "../ui/select";
-import { Lock, Share } from "lucide-react";
+'use client';
+import { useSupabaseUser } from '@/lib/providers/supabase-user-provider';
+import { User, workspace } from '@/lib/supabase/supabase.types';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
+import { Label } from '../ui/label';
+import { Input } from '../ui/input';
+import { useToast } from '../ui/use-toast';
 
 const WorkspaceCreator = () => {
   const { user } = useSupabaseUser();
   const { toast } = useToast();
   const router = useRouter();
-  const [permissions, setPermissions] = useState("private");
-  const [title, setTitle] = useState("");
+  const [permissions, setPermissions] = useState('private');
+  const [title, setTitle] = useState('');
   const [collaborators, setCollaborators] = useState<User[]>([]);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -36,7 +27,10 @@ const WorkspaceCreator = () => {
   return (
     <div className="flex gap-4 flex-col">
       <div>
-        <Label htmlFor="name" className="text-sm text-muted-foreground">
+        <Label
+          htmlFor="name"
+          className="text-sm text-muted-foreground"
+        >
           Name
         </Label>
         <div
@@ -56,57 +50,6 @@ const WorkspaceCreator = () => {
           />
         </div>
       </div>
-      <>
-        <Label
-          htmlFor="permissions"
-          className="text-sm
-          text-muted-foreground"
-        >
-          Permission
-        </Label>
-        <Select
-          onValueChange={(val) => {
-            setPermissions(val);
-          }}
-          defaultValue={permissions}
-        >
-          <SelectTrigger className="w-full h-26 -mt-3">
-            <SelectValue />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectItem value="private">
-                <div
-                  className="p-2
-                  flex
-                  gap-4
-                  justify-center
-                  items-center
-                "
-                >
-                  <Lock />
-                  <article className="text-left flex flex-col">
-                    <span>Private</span>
-                    <p>
-                      Your workspace is private to you. You can choose to share
-                      it later.
-                    </p>
-                  </article>
-                </div>
-              </SelectItem>
-              <SelectItem value="shared">
-                <div className="p-2 flex gap-4 justify-center items-center">
-                  <Share></Share>
-                  <article className="text-left flex flex-col">
-                    <span>Shared</span>
-                    <span>You can invite collaborators.</span>
-                  </article>
-                </div>
-              </SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-      </>
     </div>
   );
 };
