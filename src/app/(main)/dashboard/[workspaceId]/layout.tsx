5c1cfb82fd9e0122abd3ae2456a01fa8957a9b06
Bigyan Darshan Patel <bigyanpatel07769@gmail.com>
2024-01-15 19:51:33 +0530

    Style: fix scrollbar default for page layout

--- src/app/(main)/dashboard/[workspaceId]/layout.tsx
+++ src/app/(main)/dashboard/[workspaceId]/layout.tsx
@@ -20,7 +20,7 @@ const Layout: React.FC<LayoutProps> = ({ children, params }) => {
         border-l-[1px]
         w-full
         relative
-        overflow-scroll
+        overflow-auto
       "
       >
         {children}
